a564c1b73d636796aa1c7976df03bce81df38148
dargles <d.argles@gmx.com>
2021-05-10 19:30:21 +0100

    Get webserver working
    1) Manage connection to WiFi hub
    2) Wait for web connection, then print out "bat" parameter

--- weatherDisplay.ino
+++ weatherDisplay.ino
@@ -8,13 +8,11 @@
  */
 #define PROGRAM "weatherDisplay"
 #define VERSION "1.0"
-#define BUILD   "09 May 2021 @13:45h"
+#define BUILD   "10 May 2021 @15:58h"
 
 // Library includes
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-//#include <ESP8266WiFiMulti.h>
-#include <ESP8266HTTPClient.h>
 
 // Includes for our own files
 #include "flashscreen.h"
@@ -24,7 +22,9 @@
 // Instantiate the various classes
 flashscreen       flash;
 terminal          display;
-//ESP8266WiFiMulti  WiFiMulti;
+
+// Start the webserver (note, this is before starting the WiFi!)
+WiFiServer webserver(80);
 
 void setup() {
   Serial.begin(115200);
@@ -33,84 +33,55 @@ void setup() {
   display.print("\nProgram:\n");
   display.print(PROGRAM);
 
+  // Configure the WiFi
   WiFi.hostname(WIFI_HOSTNAME);
   WiFi.mode(WIFI_STA);
+  // Start up the WiFi
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  display.print("\nConnecting\n");  
+  // Wait for the WiFi to get going
+  display.print("\nStarting up");
+  for(int i=4;i>0;i--){
+    display.print(".");
+    delay(1000);
+  }
+  // Check the WiFi is going, wait a bit longer if necessary
+  display.print("\nConnecting");  
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     Serial.flush();
     delay(1000);
   }
-  String ipAddr = "IP:"+WiFi.localIP().toString();
+  webserver.begin();
+  display.print("\nServer begun");
+  String ipAddr = "\nIP:"+WiFi.localIP().toString();
   display.print(ipAddr);
 }
 
 void loop() {
-  // wait for WiFi connection
-  if ((WiFi.status() == WL_CONNECTED)) {
-
-    WiFiClient client;
-    HTTPClient http; //must be declared after WiFiClient for correct destruction order, because used by http.begin(client,...)
-
-    Serial.print("[HTTP] begin...\n");
-
-    // configure server and url
-    http.begin(client, "http://jigsaw.w3.org/HTTP/connection.html");
-    //http.begin(client, "jigsaw.w3.org", 80, "/HTTP/connection.html");
-
-    Serial.print("[HTTP] GET...\n");
-    // start connection and send HTTP header
-    int httpCode = http.GET();
-    if (httpCode > 0) {
-      // HTTP header has been send and Server response header has been handled
-      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-
-      // file found at server
-      if (httpCode == HTTP_CODE_OK) {
-
-        // get length of document (is -1 when Server sends no Content-Length header)
-        int len = http.getSize();
-
-        // create buffer for read
-        uint8_t buff[128] = { 0 };
-
-#if 0
-        // with API
-        Serial.println(http.getString());
-#else
-        // or "by hand"
-
-        // get tcp stream
-        WiFiClient * stream = &client;
-
-        // read all data from server
-        while (http.connected() && (len > 0 || len == -1)) {
-          // read up to 128 byte
-          int c = stream->readBytes(buff, std::min((size_t)len, sizeof(buff)));
-          Serial.printf("readBytes: %d\n", c);
-          if (!c) {
-            Serial.println("read timeout");
-          }
-
-          // write it to Serial
-          Serial.write(buff, c);
-
-          if (len > 0) {
-            len -= c;
-          }
-        }
-#endif
-
-        Serial.println();
-        Serial.print("[HTTP] connection closed or file end.\n");
-      }
-    } else {
-      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-    }
-
-    http.end();
+  // Check if a client has connected
+  WiFiClient client = webserver.available();
+  if (!client){
+    return;
   }
+  display.print("\nIncoming:\n");
 
-  delay(60000);
+  client.setTimeout(5000); // default is 1000
+
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  // display.print(req);
+  int start = req.indexOf("&bat=");
+  int end = req.indexOf("&temp=");
+  //Serial.print(index);
+  String val = req.substring(start+5,end);
+  display.print(val);
+
+  // Read rest of request
+  while (client.available()) client.read();
+
+  // Respond to client
+  client.print(F("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nTa!</html>"));
+  // As we end, the server will automatically disconnect
+  // display.print("\nDisconnecting");
+
 }
