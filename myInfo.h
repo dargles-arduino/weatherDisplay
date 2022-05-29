e4d33c72aaf5a9ed1b1443bb3c9d0e7480858180
dargles <d.argles@gmx.com>
2022-05-29 18:20:56 +0100

    Delete myInfo.h

--- myInfo.h
+++
@@ -1,8 +0,0 @@
-/**
- * myInfo.h
- * This is simply a header file that allows me to make the rest of this code 
- * public without revelaing my personal WiFi details
- */
-#define WIFI_SSID     "PLUSNET-C8R8"
-#define WIFI_PASSWORD "36d452c82c"
-#define WIFI_HOSTNAME "ESP8266-TTGO"
